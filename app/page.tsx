5c7c38663eb6b443c671d1e270db842053863cf1
Sanjay Barman <sanjaybarman870@gmail.com>
2023-05-15 12:13:34 +0530

    initial commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,24 @@
 "use client";
-import React from "react";
-import { Stage, Layer, Rect, Text, Circle, Line } from "react-konva";
+import dynamic from "next/dynamic";
+import React, { useState, useEffect } from "react";
+const DynamicKonvaComponents = dynamic(
+  async () => {
+    const data = await import("react-konva");
+    return data.Stage;
+  },
+  { ssr: false }
+);
 
-const App = () => {
+const Hello = () => {
+  const [loading, setLoading] = useState(false);
+  const [Stage, setStage] = useState(null);
+
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text='Some text on canvas' fontSize={15} />
-        <Rect
-          x={20}
-          y={50}
-          width={100}
-          height={100}
-          fill='red'
-          shadowBlur={10}
-        />
-        <Circle x={200} y={100} radius={50} fill='green' />
-        <Line
-          x={20}
-          y={200}
-          points={[0, 0, 100, 0, 100, 100]}
-          tension={0.5}
-          closed
-          stroke='black'
-          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-          fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
-        />
-      </Layer>
-    </Stage>
+    <div>
+      <h1>hh</h1>
+      <h1 onClick={() => DynamicKonvaComponents}>dsds</h1>
+    </div>
   );
 };
 
-export default App;
+export default Hello;
