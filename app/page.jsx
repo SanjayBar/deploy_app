279cfe2951b3caded49332527c2a4679b61c79c4
Sanjay Barman <sanjaybarman870@gmail.com>
2023-05-15 14:56:39 +0530

    ahahahaahaha

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,65 @@
 "use client";
-import { Stage, Layer, Rect, Circle } from "react-konva";
+import React from "react";
+
+import { useState, useRef, useEffect } from "react";
+import { Stage, Layer, Transformer, Image, Rect, Circle } from "react-konva";
+
+const ResizableImage = ({}) => {
+  const imageRef = useRef(null);
+  const transformerRef = useRef(null);
+  const [isSelected, setIsSelected] = useState(false);
+  const [image, setImage] = useState(null);
+
+  const imageobj = new window.Image();
+  imageobj.src = "https://konvajs.org/assets/yoda.jpg";
+
+  imageobj.onload = () => {
+    setImage(imageobj);
+  };
+
+  const handleSelect = () => {
+    setIsSelected(true);
+  };
+
+  const handleDeselect = () => {
+    setIsSelected(false);
+  };
+
+  if (!image) {
+    return null;
+  }
+
+  return (
+    <>
+      <Image
+        image={image}
+        onClick={handleSelect}
+        onTap={handleSelect}
+        onDblClick={handleDeselect}
+        onDblTap={handleDeselect}
+        width={100}
+        height={100}
+        draggable
+        ref={imageRef}
+      />
+      {isSelected && (
+        <Transformer
+          ref={transformerRef}
+          node={imageRef.current}
+          rotateEnabled={false}
+          keepRatio={false}
+          boundBoxFunc={(oldBox, newBox) => {
+            // Limit the minimum size of the image
+            if (newBox.width < 20 || newBox.height < 20) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </>
+  );
+};
 
 const App = () => {
   return (
@@ -8,8 +68,9 @@ const App = () => {
     // Rect and Circle are not DOM elements. They are 2d shapes on canvas
     <Stage width={500} height={500}>
       <Layer>
-        <Rect width={50} height={50} fill='red' />
-        <Circle x={200} y={200} stroke='black' radius={50} />
+        <Rect width={50} height={50} fill='red' draggable />
+        <Circle x={200} y={200} stroke='black' radius={50} draggable />
+        <ResizableImage />
       </Layer>
     </Stage>
   );
